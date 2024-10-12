a60cb00aa45da0e8a2d7eab84f548162899b07a8
Rando 87 <68273711+tharunyogenthra@users.noreply.github.com>
2024-10-12 19:13:14 +1100

    Component1 functionality (#2)
    * Added tests for the premade Test_folder_GetFoldersByOrgID which all pass and then made tests for Test_folder_GetAllChildFolders which are commented out because I have no functionality yet
    
    * Changed up the tests a bit to include error library which seems very useful
    
    * Made the core functionality. Instead of making a tree like my first plan was i found it a lot simpler to use basic string manipulation
    
    ---------
    
    Co-authored-by: Tharun Yogenthra <z5480907@ad.unsw.edu.au>

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,6 +1,9 @@
 package folder
 
-import "github.com/gofrs/uuid"
+import (
+	"errors"
+	"github.com/gofrs/uuid"
+)
 
 func GetAllFolders() []Folder {
 	return GetSampleData()
@@ -20,8 +23,72 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 
 }
 
-func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
-	// Your code here...
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
 
-	return []Folder{}
+	exists := f.CheckFolderExists(name)
+	if !exists {
+		return nil, errors.New("Error: Folder does not exist")
+	}
+
+	existsOrg := f.CheckFolderExistsWithinOrg(orgID, name)
+	if !existsOrg {
+		return nil, errors.New("Error: Folder does not exist in the specified organization")
+	}
+
+	rootPath := ""
+	folderOrgID := f.GetFoldersByOrgID(orgID)
+
+	for _, folder := range folderOrgID {
+		if folder.Name == name {
+			rootPath = folder.Paths
+			break
+		}
+	}
+
+	if rootPath == "" {
+		return []Folder{}, nil
+	}
+
+	children := []Folder{}
+	for _, folder := range f.folders {
+		if IsChildFolder(folder, rootPath) {
+			children = append(children, folder)
+		}
+	}
+
+	return children, nil
+}
+
+func (f *driver) CheckFolderExists(name string) bool {
+	for _, folder := range f.folders {
+		if folder.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *driver) CheckFolderExistsWithinOrg(orgID uuid.UUID, name string) bool {
+	folders := f.GetFoldersByOrgID(orgID)
+
+	for _, folder := range folders {
+		if folder.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func IsChildFolder(folder Folder, rootPath string) bool {
+	// as its a subfolder this condition must apply
+	if len(rootPath) >= len(folder.Paths) {
+		return false
+	}
+	// :len(rootPath) uses string splicing very similar to python
+	// x := "bravo.charlie"
+	// fmt.Println(x[:len(root)]) -> bravo
+	if folder.Paths != rootPath && folder.Paths[:len(rootPath)] == rootPath {
+		return true
+	}
+	return false
 }
