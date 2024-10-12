ed91d938cbe3684c1eabd9fd424b14ac913ec1e4
Tharun Yogenthra <z5480907@ad.unsw.edu.au>
2024-10-12 18:38:58 +1100

    Changed up the tests a bit to include error library which seems very useful

--- folder/get_folder_test.go
+++ folder/get_folder_test.go
@@ -1,11 +1,11 @@
 package folder_test
 
 import (
-	"testing"
-
+	"errors"
 	"github.com/georgechieng-sc/interns-2022/folder"
 	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 // This data is provided in the spec for component 1
@@ -99,69 +99,85 @@ func Test_folder_GetFoldersByOrgID(t *testing.T) {
 	}
 }
 
-// func Test_folder_GetAllChildFolders(t *testing.T) {
-// 	t.Parallel()
-// 	// reuse to save performace
-// 	orgID := uuid.FromStringOrNil("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")
-// 	tests := [...]struct {
-// 		name_of_test   string
-// 		orgID          uuid.UUID
-// 		name_of_folder string
-// 		folders        []folder.Folder
-// 		want           []folder.Folder
-// 	}{
-// 		{
-// 			name_of_test:   "Gets all child folders from org and name",
-// 			orgID:          orgID,
-// 			name_of_folder: "alpha",
-// 			folders:        GetTestingSampleData(),
-// 			want: []folder.Folder{
-// 				{Name: "bravo", OrgId: orgID, Paths: "alpha.bravo"},
-// 				{Name: "charlie", OrgId: orgID, Paths: "alpha.bravo.charlie"},
-// 				{Name: "delta", OrgId: orgID, Paths: "alpha.delta"},
-// 			},
-// 		},
-// 		{
-// 			name_of_test:   "Gets all child folders a child folder",
-// 			orgID:          orgID,
-// 			name_of_folder: "bravo",
-// 			folders:        GetTestingSampleData(),
-// 			want: []folder.Folder{
-// 				{
-// 					Name:  "charlie",
-// 					OrgId: orgID,
-// 					Paths: "alpha.bravo.charlie",
-// 				},
-// 			},
-// 		},
-// 		{
-// 			name_of_test:   "Given folder has no child folders",
-// 			orgID:          orgID,
-// 			name_of_folder: "charlie",
-// 			folders:        GetTestingSampleData(),
-// 			want:           []folder.Folder{},
-// 		},
-// 		{
-// 			name_of_test:   "Given folder has no child folders",
-// 			orgID:          orgID,
-// 			name_of_folder: "echo",
-// 			folders:        GetTestingSampleData(),
-// 			want:           "Error: Folder does not exist",
-// 		},
-// 		{
-// 			name_of_test:   "Given folder doesnt exist",
-// 			orgID:          orgID,
-// 			name_of_folder: "invalid_folder",
-// 			folders:        GetTestingSampleData(),
-// 			want:           "Error: Folder does not exist in the specified organization",
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name_of_test, func(t *testing.T) {
-// 			f := folder.NewDriver(tt.folders)
-// 			got := f.GetAllChildFolders(tt.orgID, tt.name_of_folder)
+func Test_folder_GetAllChildFolders(t *testing.T) {
+	t.Parallel()
+	orgID := uuid.FromStringOrNil("38b9879b-f73b-4b0e-b9d9-4fc4c23643a7")
+	tests := [...]struct {
+		name_of_test   string
+		orgID          uuid.UUID
+		name_of_folder string
+		folders        []folder.Folder
+		want           []folder.Folder
+		wantErr        error
+	}{
+		{
+			name_of_test:   "Gets all child folders from org and name",
+			orgID:          orgID,
+			name_of_folder: "alpha",
+			folders:        GetTestingSampleData(),
+			want: []folder.Folder{
+				{Name: "bravo", OrgId: orgID, Paths: "alpha.bravo"},
+				{Name: "charlie", OrgId: orgID, Paths: "alpha.bravo.charlie"},
+				{Name: "delta", OrgId: orgID, Paths: "alpha.delta"},
+			},
+			wantErr: nil,
+		},
+		{
+			name_of_test:   "Gets all child folders of a child folder",
+			orgID:          orgID,
+			name_of_folder: "bravo",
+			folders:        GetTestingSampleData(),
+			want: []folder.Folder{
+				{
+					Name:  "charlie",
+					OrgId: orgID,
+					Paths: "alpha.bravo.charlie",
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name_of_test:   "Given folder has no child folders",
+			orgID:          orgID,
+			name_of_folder: "charlie",
+			folders:        GetTestingSampleData(),
+			want:           []folder.Folder{},
+			wantErr:        nil,
+		},
+		{
+			name_of_test:   "Given folder doesn't exist",
+			orgID:          orgID,
+			name_of_folder: "invalid_folder",
+			folders:        GetTestingSampleData(),
+			want:           []folder.Folder{},
+			wantErr:        errors.New("Error: Folder does not exist"),
+		},
+		{
+			name_of_test:   "Given folder does not exist in org",
+			orgID:          orgID,
+			name_of_folder: "echo",
+			folders:        GetTestingSampleData(),
+			want:           []folder.Folder{},
+			wantErr:        errors.New("Error: Folder does not exist in the specified organization"),
+		},
+	}
 
-// 			assert.Equal(t, tt.want, got, "The expected output doesnt match")
-// 		})
-// 	}
-// }
+	for _, tt := range tests {
+		t.Run(tt.name_of_test, func(t *testing.T) {
+			f := folder.NewDriver(tt.folders)
+			got, err := f.GetAllChildFolders(tt.orgID, tt.name_of_folder)
+
+			if err != nil && tt.wantErr != nil {
+				assert.Equal(t, tt.wantErr.Error(), err.Error(), "Error message does not match")
+				return
+			}
+
+			if err != nil {
+				t.Errorf("GetAllChildFolders() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			assert.Equal(t, tt.want, got, "The expected output doesn't match")
+		})
+	}
+}
